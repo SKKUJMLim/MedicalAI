be403f859893cc7040a981122467a0d481831eec
임종민 <skku.jmlim@gmail.com>
2024-09-10 14:02:59 +0900

    Resnet에 Attention 추가

--- models/attention.py
+++ models/attention.py
@@ -20,18 +20,16 @@ class ChannelAttention(nn.Module):
         return self.sigmoid(out)
 
 
-# Spatial Attention Module
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
-        assert kernel_size in (3, 7), 'Kernel size must be 3 or 7'
-        padding = 3 if kernel_size == 7 else 1
-        self.conv1 = nn.Conv2d(2, 1, kernel_size=kernel_size, padding=padding, bias=False)
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size // 2, bias=False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)  # Average pooling along channel axis
-        max_out, _ = torch.max(x, dim=1, keepdim=True)  # Max pooling along channel axis
-        x = torch.cat([avg_out, max_out], dim=1)  # Concatenate along the channel axis
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
         x = self.conv1(x)
         return self.sigmoid(x)
