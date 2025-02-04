6e752eeea73ee1bc456a887fb148866f2d8c3a07
SHJangskku <rainfall001202@gmail.com>
2025-02-04 16:10:24 +0900

    Modify combinedModel.py for [add combinedUnet]

--- models/combinedModel.py
+++ models/combinedModel.py
@@ -83,3 +83,34 @@ class CombinedVGG(nn.Module):
             output = self.fc(combined)
 
             return output
+class CombinedUnet(nn.Module):
+    def __init__(self, model1, model2, model3 ,num_classes):
+        super(CombinedUnet, self).__init__()
+        self.model1 = model1
+        self.model2 = model2
+        self.model3 = model3
+
+        # 두 모델의 출력 크기를 합친 크기의 FC 레이어
+        self.fc = nn.Linear(512 * 2 + 1, num_classes) # Resnet 18, 34
+        # self.fc = nn.Linear(512 * 2, num_classes)  # Resnet 18, 34
+        # self.fc = nn.Linear(512 * 8 + 1, num_classes)   # Resnet 50
+        # self.fc = nn.Linear(num_classes * 3, num_classes)
+
+    def forward(self, x1, x2, x3):
+            # 모델 각각에 이미지를 입력하여 latent vector 추출
+            latent_vector1 = self.model1(x1)
+            # print(latent_vector1.shape)(8,1,512,512)
+            latent_vector1.squeeze()
+            latent_vector2 = self.model2(x2)
+            latent_vector2.squeeze()
+            # 환자 정보 이미지
+            latent_vector3 = self.model3(x3)
+            #print(latent_vector3.shape)
+            # 두 latent vector를 concat
+            # print(latent_vector1.shape , latent_vector2.shape, latent_vector3.shape)
+            combined = torch.cat((latent_vector1, latent_vector2, latent_vector3), dim=1)
+
+            # 결합된 벡터를 최종 분류 레이어에 통과시킴
+            output = self.fc(combined)
+
+            return output
